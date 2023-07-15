8e17fdaed824f24b18eac0d3432c615734412f1a
Akash Phayade <76113440+AkashP27@users.noreply.github.com>
2023-07-15 17:17:06 +0530

    deleting constants file

--- src/main/java/com/springSecurityJWT/constants/SecurityConstants.java
+++
@@ -1,7 +0,0 @@
-package com.springSecurityJWT.constants;
-
-public class SecurityConstants {
-
-    public static final String JWT_SECRET_KEY = "jxgEQeXHuPq8VdbyYFNkANdudQ53YUn4";
-    public static final String JWT_HEADER = "Authorization";
-}
